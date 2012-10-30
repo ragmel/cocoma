9f79638a15c08739eefb86fdad2b69cda32b0dfc
Sergej Svorobej <s.svorobej@sap.com>
2012-10-30 14:36:56 +0000

    API GET Mockup

--- bin/ccmshAPI.py
+++ bin/ccmshAPI.py
@@ -22,35 +22,86 @@
 
 from bottle import route, run,response,request
 import sys,os
-
-import EmulationManager,ccmsh
+from datetime import datetime as dt
+import EmulationManager,ccmsh,DistributionManager
 from json import dumps
 
 try:
     HOMEPATH= os.environ['COCOMA']
 except:
     print "no $COCOMA environmental variable set"
+CONTENT = "application/vnd.cocoma+xml"
 
 
-@route('/ccmsh')
-def xml():
-    response.status = 404
-    response.headers['status'] = response.status#str(response.status_code())
-    response.content_type = "application/vnd.bonfire+xml"
-    
-    
-    xml_content_whatever = '''<?xml version="1.0" encoding="utf-8"?>
-    <root xmlns="http://111" href="/">
-        <version>195</version>
-        <timestamp>1234</timestamp>
-        <link rel="emulations" href="/emulations" type="application/vnd.bonfire+xml"/>
-    </root>
+'''
+#######
+COCOMA ROOT
+#######
+'''
+@route('/', method ="GET")
+def get_root():
+    response.content_type = CONTENT
+    #curl -k -i http://10.55.164.211:8050/
+    xml_content_root = '''<?xml version="1.0" encoding="UTF-8"?>
+<root xmlns="http://api.bonfire-project.eu/doc/schemas/cocoma" href="/">
+  <version>0.3</version>
+  <timestamp>'''+str(DistributionManager.timestamp(dt.now()))[0:-2]+'''</timestamp>
+  <link rel="emulations" href="/emulations" type="application/vnd.cocoma+xml"/>
+  <link rel="emulators" href="/emulators" type="application/vnd.cocoma+xml"/>
+  <link rel="distributions" href="/distributions" type="application/vnd.cocoma+xml"/>
+</root>
     '''
-    return xml_content_whatever
 
-@route('/ccmsh/list')
-def get_emulation():
+    return xml_content_root    
+    
+    
+
+'''
+#######
+GET emulation
+#######
+'''
+
+@route('/emulations', method ='GET')
+def get_emulations():
+    response.content_type = CONTENT
+    '''
+    ##############
+    DUMMY
+    ##############
+    '''
+    #curl -k -i http://10.55.164.211:8050/emulations/
+    xml_content_emulations=''' <?xml version="1.0" encoding="UTF-8"?>
+<collection xmlns="http://api.bonfire-project.eu/doc/schemas/cocoma" href="/emulations">
+  <items offset="0" total="1">
+<emulation href="/emulations/1">
+  <id>1</id>
+  <emulationName>myLinearEmu</emulationName>
+  <emulationType>Contentious</emulationType>
+  <resourceType>CPU</resourceType>
+  <startTime>2012-10-23T10:30:00</startTime>
+  <stopTime>2012-10-23T10:31:00</stopTime>
+  <emulators>
+    <emulator href="/emulations/1/stressapptest" name="stressapptest"/>
+  </emulators>
+  <distributions>
+    <distribution href="/emulations/1/linear" name="linear"/>
+  </distributions>
+  <link rel="parent" href="/"/>
+  <link rel="emulators" href="/emulations/1/emulators"/>
+  <link rel="distributions" href="/emulations/1/distributions"/>
+</emulation>
+  </items>
+  <link href="/" rel="parent" type="application/vnd.cocoma+xml"/>
+</collection>
+    '''    
+    
+    return xml_content_emulations  
+    
+    
+    '''
     emulationSelect=EmulationManager.getEmulation("NULL","NULL",1,"NULL")
+    print emulationSelect
     
     if emulationSelect:
             
@@ -62,13 +113,146 @@ def get_emulation():
         
         #return "emulation ID: \"",emulationID,"\" does not exists"
         return {"success":False, "error": "No emulations found"}
+    '''
+
+
+@route('/emulations/<ID>', method='GET')
+def get_emulation(ID=""):
+    response.content_type = CONTENT
+    #curl -k -i http://10.55.164.211:8050/emulations/1
+
+    xml_content_emulationsID= '''<?xml version="1.0" encoding="UTF-8"?>
+<emulation xmlns="http://api.bonfire-project.eu/doc/schemas/cocoma" href="/emulations/1">
+  <id>'''+ID+'''</id>
+  <emulationName>myLinearEmu</emulationName>
+  <emulationType>Contentious</emulationType>
+  <resourceType>CPU</resourceType>
+  <startTime>2012-10-23T10:30:00</startTime>
+  <stopTime>2012-10-23T10:31:00</stopTime>
+  <emulators>
+    <emulator href="/emulations/1/stressapptest" name="stressapptest"/>
+  </emulators>
+  <distributions>
+    <distribution href="/emulations/1/linear" name="linear"/>
+  </distributions>
+  <link rel="parent" href="/"/>
+  <link rel="emulators" href="/emulations/1/emulators"/>
+  <link rel="distributions" href="/emulations/1/distributions"/>
+  <link href="/" rel="parent" type="application/vnd.cocoma+xml"/>
+</emulation>
+    '''
+    return xml_content_emulationsID
+    
+    
+    
+    
+    
+    '''
+    xml = request.forms.get( "xml" )
+    e = request.forms.get("e")
+    ret = str(e)+str(xml)+"\n"
+    return  ret
+    '''
+
+
+@route('/emulations/<ID>/emulators', method='GET')
+def get_emulators(ID=""):
+    response.content_type = CONTENT
+    #curl -k -i http://10.55.164.211:8050/emulations/1/emulators
+    xml_content_emulators ='''<?xml version="1.0" encoding="UTF-8"?>
+<collection xmlns="http://api.bonfire-project.eu/doc/schemas/cocoma" href="/emulations/1/emulators">
+  <items offset="0" total="1">
+<emulator href="/emulations/1/emulators/stressapptest" name="stressapptest"/>
+  </items>
+  <link href="/emulations/stressaptest" rel="parent" type="application/vnd.cocoma+xml"/>
+</collection>
+    '''
+    return xml_content_emulators
+
+
+
+
+@route('/emulations/<ID>/emulators/<name>', method='GET')
+def get_emulator(ID="",name=""):
+    response.content_type = CONTENT
+    #curl -k -i http://10.55.164.211:8050/emulations/1/emulators/stressapptest
+    xml_content_emulatorName = '''<?xml version="1.0" encoding="UTF-8"?>
+<emulator xmlns="http://api.bonfire-project.eu/doc/schemas/cocoma" href="/emulations/1/emulators/stressapptest">
+<name>'''+name+'''</name>
+   <info>
+        Stats: SAT revision 1.0.3_autoconf, 32 bit binary
+        Log: buildd @ biber on Thu Jul 29 21:47:10 UTC 2010 from open source release
+        Usage: ./sat(32|64) [options]
+        ...
+   </info>
+</emulator>
+    '''
+    
+    return xml_content_emulatorName
+
+@route('/emulations/<ID>/distributions', method='GET')
+def get_distributions(ID=""):
+    response.content_type = CONTENT
+    #curl -k -i http://10.55.164.211:8050/emulations/1/distributions
+    xml_content_distributions ='''<?xml version="1.0" encoding="UTF-8"?>
+<collection xmlns="http://api.bonfire-project.eu/doc/schemas/cocoma" href="/emulations/1/emulators">
+  <items offset="0" total="1">
+<emulator href="/emulations/1/distributions/linear" name="linear"/>
+  </items>
+  <link href="/distributions/linear" rel="parent" type="application/vnd.cocoma+xml"/>
+</collection>
+    '''
+    return xml_content_distributions
+
+
+@route('/emulations/<ID>/distributions/<name>', method='GET')
+def get_distribution(ID="", name=""):
+    response.content_type = CONTENT
+    #curl -k -i http://10.55.164.211:8050/emulations/1/distributions/linear
+    xml_content_distributionName = '''<?xml version="1.0" encoding="UTF-8"?>
+<distribution xmlns="http://api.bonfire-project.eu/doc/schemas/cocoma" href="/emulations/1/distributions/linear">
+<name>'''+name+'''</name>
+   <info>
+        Stats: SAT revision 1.0.3_autoconf, 32 bit binary
+        Log: buildd @ biber on Thu Jul 29 21:47:10 UTC 2010 from open source release
+        Usage: ./sat(32|64) [options]
+        ...
+   </info>
+</emulator>
+    '''
+    return xml_content_distributionName
+
+
+
     
 @route('/ccmsh/hello')
 def api_status():
+    response.status = 200
+    response.headers['status'] = response.status#str(response.status_code())
+    response.content_type = "application/vnd.cocoma+xml"
     return "Yes, Hello this is ccmshAPI."
 
 
-@route('/ccmsh/create')
+
+'''
+#######
+Creating emulation
+#######
+'''
+
+@route('/emulations/<ID>', method='PUT')
+def create_emu(ID=""):
+    xml = request.forms.get( "xml" )
+    e = request.forms.get("e")
+    ret = str(e)+str(xml)+"\n"
+    return  ret
+    
+
+
+
+
+
+@route('/ccmsh/create', method='PUT')
 def create_emulation():
         
         #checking for daemon
@@ -77,7 +261,7 @@ def create_emulation():
         else:
             d={}
             
-                         
+                                     
             emulationName = request.query.get('emulationName')
             distributionType = request.query.get('distributionType')
             resourceType = request.query.get('resourceType')
